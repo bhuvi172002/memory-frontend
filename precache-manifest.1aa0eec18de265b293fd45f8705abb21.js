26e50a975caddfe2f122de7f2c6706410c6d76e9
bhuvi172002 <abhuvi1718@gmail.com>
2024-10-17 00:54:06 +0530

    gvd

---
+++ precache-manifest.1aa0eec18de265b293fd45f8705abb21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f147733754a2d59d29514c1f48edeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37037af0994ecd752e9f",
+    "url": "/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "5664b7ba1214f7ecbcae",
+    "url": "/static/css/main.ee369c34.chunk.css"
+  },
+  {
+    "revision": "37037af0994ecd752e9f",
+    "url": "/static/js/2.b829f623.chunk.js"
+  },
+  {
+    "revision": "ecc2d941d7ab639758dd9d6dc4d621ce",
+    "url": "/static/js/2.b829f623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5664b7ba1214f7ecbcae",
+    "url": "/static/js/main.badd6027.chunk.js"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  }
+]);
